0c97c567cb97c98f12f3fd02762c19d6cce9499d
Yiran Huang <yiranh97@gmail.com>
2024-09-18 02:28:42 +0000

    Short GPT

---
+++ ShortGPT/run_bunny.sh
@@ -0,0 +1,11 @@
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name BAAI/Bunny-v1_0-3B --num_examples 50 --n_prune_layers 5 --device cuda:1
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name BAAI/Bunny-v1_0-3B --num_examples 50 --n_prune_layers 10 --device cuda:1
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name BAAI/Bunny-v1_0-3B --num_examples 50 --n_prune_layers 15 --device cuda:1
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name BAAI/Bunny-v1_0-3B --num_examples 50 --n_prune_layers 21 --device cuda:1
+
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name BAAI/Bunny-v1_0-3B --num_examples 100 --n_prune_layers 5 --device cuda:1
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name BAAI/Bunny-v1_0-3B --num_examples 100 --n_prune_layers 10 --device cuda:1
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name BAAI/Bunny-v1_0-3B --num_examples 100 --n_prune_layers 15 --device cuda:1
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name BAAI/Bunny-v1_0-3B --num_examples 100 --n_prune_layers 21 --device cuda:1
+
+
---
+++ ShortGPT/run_llava.sh
@@ -0,0 +1,11 @@
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name liuhaotian/llava-v1.5-7b --num_examples 50 --n_prune_layers 5 --device cuda:2
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name liuhaotian/llava-v1.5-7b --num_examples 50 --n_prune_layers 10 --device cuda:2
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name liuhaotian/llava-v1.5-7b --num_examples 50 --n_prune_layers 15 --device cuda:2
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name liuhaotian/llava-v1.5-7b --num_examples 50 --n_prune_layers 21 --device cuda:2
+
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name liuhaotian/llava-v1.5-7b --num_examples 100 --n_prune_layers 5 --device cuda:2
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name liuhaotian/llava-v1.5-7b --num_examples 100 --n_prune_layers 10 --device cuda:2
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name liuhaotian/llava-v1.5-7b --num_examples 100 --n_prune_layers 15 --device cuda:2
+python /shared-local/aoq609/VLMCompression/ShortGPT/short_gpt/prune.py --model_name liuhaotian/llava-v1.5-7b --num_examples 100 --n_prune_layers 21 --device cuda:2
+
+
